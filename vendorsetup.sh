89b853d647f29f34569e7beac018e65c9cb7ed38
valerydelinois <124895232+valerydelinois@users.noreply.github.com>
2024-02-24 09:22:19 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a32x-user
-add_lunch_combo omni_a32x-userdebug
-add_lunch_combo omni_a32x-eng
+COMMON_LUNCH_CHOICES omni_a32x-user
+COMMON_LUNCH_CHOICES omni_a32x-userdebug
+COMMON_LUNCH_CHOICES omni_a32x-eng
