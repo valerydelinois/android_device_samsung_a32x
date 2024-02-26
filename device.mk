10bcfc3ecbb80936ff87c62e0e08288918587f90
valerydelinois <valerydelinois@gmail.com>
2024-02-25 19:35:29 -0500

    v5

--- device.mk
+++ device.mk
@@ -6,3 +6,21 @@
 #
 
 LOCAL_PATH := device/samsung/a32x
+
+# Added by Varoo
+#Dynamic Partition Flag
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Apex Libraries
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
+
+TW_EXCLUDE_APEX := true
